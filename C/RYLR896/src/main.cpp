ada83a46a5b99f6c6053fe6299aa9efc9652d8a4
hunt3041 <hunt3041@gmail.com>
2024-04-02 12:17:15 -0500

    Transmitter radio code is complete. Need to implement onto robot

--- C/RYLR896/src/main.cpp
+++ C/RYLR896/src/main.cpp
@@ -5,23 +5,40 @@ const int led = 25;
 const int TX = 8;
 const int RX = 9;
 
+const int Button = 2;
+
+
+
 void radioSetup(){
   Serial2.println("AT+RESET");
-  delay(1000);
-  Serial2.println("AT+ADDRESS=18");
-  delay(1000);
-  Serial2.println("AT+NETWORKID=4");
-  delay(1000);
-  Serial2.println("AT+BAND=86850000");
-  delay(1000);
-  Serial2.println("AT+PARAMETER=10,7,1,7");
-  delay(1000);
-  Serial2.println("AT+VER?");
-  delay(1000);
-  // Serial2.println("AT+FACTORY");
-  // Serial.print("Setting to factory settings...");
-  // Serial.println(Serial2.readString());
+  Serial.print("Response: ");
+  Serial.println(Serial2.readString());
+  Serial.println(Serial2.readString());
+  Serial2.println("AT+BAND=915000000");
+  Serial.print("Response: ");
+  Serial.println(Serial2.readString());
+  Serial2.println("AT+NETWORKID=5");
+  Serial.print("Response: ");
+  Serial.println(Serial2.readString());
+  Serial2.println("AT+ADDRESS=50");
+  Serial.print("Response: ");
+  Serial.println(Serial2.readString());
+
+  // Serial2.println("AT+RESET");
+  // Serial2.readString();
+  // Serial2.readString();
+  // Serial2.println("AT+BAND=915000000");
+  // Serial2.readString();
+  // Serial2.println("AT+NETWORKID=5");
+  // Serial2.readString();
+  // Serial2.println("AT+ADDRESS=50");
+  // Serial2.readString();
+
+
+
+
 }
+
 
 void setup() {
   Serial2.setTX(TX);
@@ -31,35 +48,37 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(led, OUTPUT);
+  pinMode(Button, INPUT);
 
   // receiver only
   while(!Serial){}
   delay(10);
 
   radioSetup();
+
 }
 
 String message;
-
-
-
-
+int buttonCount = 0;
 //TX-------------------------------------------
 
 // void loop() {
+//   if(digitalRead(Button) == HIGH){
+//     delay(300);
+//     if(buttonCount % 2 == 0){
+//       Serial2.println("AT+SEND=50,5,START");
+//       buttonCount += 1;
+//       digitalWrite(led, HIGH);
+//     }
+//     else{
+//       Serial2.println("AT+SEND=50,4,STOP");
+//       buttonCount += 1;
+//       digitalWrite(led, LOW);
+//     }
 
-  
-//   Serial2.println("AT+SEND=50,5,HELLO");
-//   digitalWrite(led, HIGH);
-//   delay(100);
-  
-//   if(Serial2.available()){
-//     message = Serial2.readString();
-//     Serial.println(message);
-    
 //   }
-//   digitalWrite(led, LOW);
-//   delay(100);
+
+
 // }
 
 
@@ -69,7 +88,8 @@ void loop(){
     Serial.println(Serial2.readString());
   }
 
-  delay(1000);
+  delay(100);
   
 }
+
 
