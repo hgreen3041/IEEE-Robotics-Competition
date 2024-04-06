1e9c0b7bc0fd10eacf35f9c2b8e79b2e158c4355
Kevin Thomas <ps2gamer20@gmail.com>
2024-04-06 03:10:30 -0500

    failed imu reset, changed it to not print

--- C/ICM 20948/src/main.cpp
+++ C/ICM 20948/src/main.cpp
@@ -62,7 +62,7 @@ void setup()
 
   Serial1.begin(115200);
 
-  Serial.begin(115200);
+  // Serial.begin(115200);    //usb serial got rid of no need to see right now.
   // while(!Serial) delay(10);
   
   
@@ -210,12 +210,12 @@ void loop()
       double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
       double yaw = atan2(t3, t4) * 180.0 / PI;
 
-      Serial.print(F("Roll:"));
-      Serial.print(roll, 1);
-      Serial.print(F(" Pitch:"));
-      Serial.print(pitch, 1);
-      Serial.print(F(" Yaw:"));
-      Serial.println(yaw, 1);
+      // Serial.print(F("Roll:"));
+      // Serial.print(roll, 1);
+      // Serial.print(F(" Pitch:"));
+      // Serial.print(pitch, 1);
+      // Serial.print(F(" Yaw:"));
+      // Serial.println(yaw, 1);
       Serial1.println(String(yaw, 1));
     }
   }
